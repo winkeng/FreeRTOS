310d1b8375a3e25279a9bd735a57e7feb34210e3
vincent <1002338144@qq.com>
2019-01-06 17:34:35 +0800

    任务创建和删除(动态方法)

--- USER/main.c
+++ USER/main.c
@@ -14,15 +14,20 @@
 
 
 
-#define START_TASK_PRIO  1      //任务优先级
-#define START_STK_SIZE   256    //任务堆栈大小
-TaskHandle_t StartTask_Handler;     //任务句柄
-void start_task(void *pvParameters);  //任务函数
+#define START_TASK_PRIO  1      		//任务优先级
+#define START_STK_SIZE   128    		//任务堆栈大小
+TaskHandle_t StartTask_Handler;    		//任务句柄
+void start_task(void *pvParameters);  	//任务函数
 
-#define INTERRUPT_TASK_PRIO   2      //任务优先级
-#define INTERRUPT_STK_SIZE    256    //任务堆栈大小
-TaskHandle_t INTERRUPTTask_Handler;  //任务句柄
-void INTERRUPT_task(void *p_arg);    //任务函数
+#define TASK1_TASK_PRIO		    2      //任务优先级
+#define TASK1_STK_SIZE		    128    //任务堆栈大小
+TaskHandle_t Task1Task_Handler;		   //任务句柄
+void task1_task(void *pvParameters);   //任务函数
+
+#define TASK2_TASK_PRIO		    3      //任务优先级
+#define TASK2_STK_SIZE		    128    //任务堆栈大小
+TaskHandle_t Task2Task_Handler;		   //任务句柄
+void task2_task(void *pvParameters);   //任务函数
 
 
 /****
@@ -34,8 +39,6 @@ int main(void)
 	delay_init();
 	uart_init(115200);					//初始化串口
 	LED_Init();
-	TIM3_Int_Init(10000-1,7200-1);		//初始化定时器3，定时器周期1S
-	TIM4_Int_Init(10000-1,7200-1);		//初始化定时器5，定时器周期1S		
 	//创建开始任务函数
 	xTaskCreate( (TaskFunction_t )start_task,         //任务函数
 							 (const char*    )"start_task",       //任务名称
@@ -54,35 +57,55 @@ int main(void)
 void start_task(void *pvParameters)
 {
     taskENTER_CRITICAL();           //进入临界区
-    //创建中断测试任务
-    xTaskCreate((TaskFunction_t )INTERRUPT_task,     	
-                (const char*    )"INTERRUPT_task",
-                (uint16_t       )INTERRUPT_STK_SIZE, 
+    //创建TASK1任务
+    xTaskCreate((TaskFunction_t )task1_task,     	
+                (const char*    )"task1_task",
+                (uint16_t       )TASK1_STK_SIZE, 
                 (void*          )NULL,				
-                (UBaseType_t    )INTERRUPT_TASK_PRIO,	
-                (TaskHandle_t*  )&INTERRUPTTask_Handler);    
+                (UBaseType_t    )TASK1_TASK_PRIO,	
+                (TaskHandle_t*  )&Task1Task_Handler);    
+    //创建TASK1任务
+    xTaskCreate((TaskFunction_t )task2_task,     	
+                (const char*    )"task2_task",
+                (uint16_t       )TASK2_STK_SIZE, 
+                (void*          )NULL,				
+                (UBaseType_t    )TASK2_TASK_PRIO,	
+                (TaskHandle_t*  )&Task2Task_Handler);    
     vTaskDelete(StartTask_Handler); //删除开始任务
     taskEXIT_CRITICAL();            //退出临界区
 }
 
 
-//中断测试任务函数
-void INTERRUPT_task(void *pvParameters)
+//task1 任务函数 
+void task1_task(void* pvParameters)
 {
-	static u32 total_num=0;
+	u8 task1_num=0;
 	while(1)
 	{
-		total_num+=1;
-		if(total_num==5)
+		task1_num++;
+		LED12 = !LED12;
+		printf("任务1已执行：%d\r\n",task1_num);
+		
+		if(task1_num==5)
 		{
-			printf("关闭中断.............\r\n");
-			portDISABLE_INTERRUPTS();				//关闭中断
-			delay_xms(5000);						//延时5s
-			printf("打开中断.............\r\n");	//打开中断
-			portENABLE_INTERRUPTS();			
+			vTaskDelete(Task2Task_Handler); //任务1执行5次删除任务2
+			printf("任务1删除了任务2!!!\r\n");
 		}
-        LED12=~LED12;
-        vTaskDelay(1000);		
+		vTaskDelay(1000);  //延时1s
+	}
+}
+
+//task2 任务函数 
+void task2_task(void* pvParameters)
+{
+	u8 task2_num=0;
+	while(1)
+	{
+		task2_num++;
+		LED13 = !LED13;
+		printf("任务2已执行：%d\r\n",task2_num);
+		
+		vTaskDelay(1000);  //延时1s
 	}
 }
 
